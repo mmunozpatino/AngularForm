71e87c1c1e92c9be4c3ee27486ed6b0259eeaedd
Mercedes <mercedesmp1305@gmail.com>
2017-07-04 12:05:21 -0300

    holaa

---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core'
+
+@Component({
+   selector: 'formulario',
+   templateUrl: 'formulario.component.html'
+})
+export class Formulario{
+   titulo= 'Formulario';
+   values = '';
+   enter: string;
+
+   onKey(event: KeyboardEvent){
+      this.values += (<HTMLInputElement>event.target).value + '|';
+   }
+   onEnter(s: string){
+      this.enter = s;
+   }
+}
+
